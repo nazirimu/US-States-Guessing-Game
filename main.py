2881bf4381cd18f5f989b307e615d191b9a47ed2
nazirimu <65688004+nazirimu@users.noreply.github.com>
2020-12-17 01:27:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ all_states = data.state.tolist()
 
 # Tracks the number of correct guesses
 correct = 0
-
+guessed_states = []
 # Loop runs until the user guesses all 50 cases
 while correct <50:
 
@@ -29,19 +29,23 @@ while correct <50:
 
     # Checks if the guess is an actual state and then displays it on the map
     if guess in all_states:
+        guessed_states.append(guess)
         x = int(data[data["state"] == guess]["x"])
         y = int(data[data["state"] == guess]["y"])
         writer.goto(x, y)
         writer.write(guess)
         correct += 1
 
+    # Breaks the while loop
+    if guess == "Exit":
+        break
 
+# Checks to see what states were missed
+states_missed = []
+for state in all_states:
+    if state not in guessed_states:
+        states_missed.append(state)
 
-
-
-
-
-
-
-
-screen.exitonclick()
+# Coverts the missed states into a data frame which is then written on to a csv file
+df = pd.DataFrame(states_missed,columns=["States"])
+df.to_csv("Missed_States")
